1dcfbe882bd597849a0b4f6ed6f9c07fb6ae01b6
PerizatSabitbek <psabitbek@magnumtech.kz>
2021-11-02 13:23:31 +0600

    add gulp-sass

--- gulpfile.js
+++ gulpfile.js
@@ -23,10 +23,12 @@ let path = {
   clean: "./" + project_folder + "/",
 };
 
-let { src, dest } = require('gulp'),
-  gulp = require('gulp'),
+let { src, dest } = require("gulp"),
+  gulp = require("gulp"),
   browsersync = require("browser-sync").create(),
-  fileinclude = require("gulp-file-include");
+  fileinclude = require("gulp-file-include"),
+  del = require("del"),
+  scss = require("gulp-sass")(require("sass"));
 
 function browserSync(params) {
   browsersync.init({
@@ -34,24 +36,41 @@ function browserSync(params) {
       baseDir: "./" + project_folder + "/",
     },
     port: 3000,
-    notify: false
-  })
+    notify: false,
+  });
 }
 
 function html() {
   return src(path.src.html)
     .pipe(fileinclude())
     .pipe(dest(path.build.html))
-    .pipe(browsersync.stream())
+    .pipe(browsersync.stream());
+}
+
+function css() {
+  return src(path.src.css)
+    .pipe(
+      scss({
+        outputStyle: "expanded",
+      })
+    )
+    .pipe(dest(path.build.css))
+    .pipe(browsersync.stream());
 }
 
 function watchFiles(params) {
   gulp.watch([path.watch.html], html);
+  gulp.watch([path.watch.css], css);
 }
 
-let build = gulp.series(html);
+function clean(params) {
+  return del(path.clean);
+}
+
+let build = gulp.series(clean,  gulp.parallel(css, html));
 let watch = gulp.parallel(browserSync, build, watchFiles);
 
+exports.css = css;
 exports.html = html;
 exports.build = build;
 exports.watch = watch;
